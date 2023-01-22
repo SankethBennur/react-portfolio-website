9bc7d1bcd014fcadcefe2087152b84186b30b9f4
SankethBennur <snkthbnnr3@gmail.com>
2023-01-23 00:28:00 +0530

    Projects and Work Experience components

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import Greetings from "./Greetings";
+import WorkExperience from "./WorkExperience";
+import Projects from "./Projects";
 
 function Contents() {
-    return (
-        <Grid xs={8} sx={{ border: 2, borderColor: "white" }}>
-            <h2 style={{ color: "turquoise" }}>__Content__</h2>
-            <Greetings />
-        </Grid>
-    );
+	return (
+		<Grid xs={8} sx={{ border: 2, borderColor: "white" }}>
+			<h2 style={{ color: "turquoise" }}>__Content__</h2>
+			<Greetings />
+			<WorkExperience />
+			<Projects />
+		</Grid>
+	);
 }
 
 export default Contents;
