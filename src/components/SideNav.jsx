c231d4539c82febeeeb3abadf0adb68e0f9eeff6
SankethBennur <snkthbnnr3@gmail.com>
2023-01-25 14:05:57 +0530

    created scrollable side-nav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,27 +1,62 @@
-import { Grid } from "@mui/material";
+import { Drawer, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import "./SideNav.css";
 import myPhoto from "../assets/images/me.jpg";
 
 function SideNav() {
-    return (
-        <Grid sx={{ border: 1 }}>
-            <Box
-                component="img"
-                sx={{ height: "50%", width: "50%" }}
-                alt="Sanketh Bennur"
-                src={myPhoto}
-            />
-            <Grid>
-                <p>loc_1</p>
-                <p>loc_2</p>
-                <p>loc_3</p>
-                <p>loc_4</p>
-                <p>loc_5</p>
-            </Grid>
-        </Grid>
-    );
+	return (
+		<Grid
+			sx={{
+				border: 1,
+				display: "flex",
+				flexDirection: "column",
+				overflowY: "auto",
+				maxHeight: "100vh",
+			}}
+		>
+			<Box
+				component="img"
+				sx={{ height: "50%", width: "50%" }}
+				alt="Sanketh Bennur"
+				src={myPhoto}
+			/>
+			<Grid>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+				<p>=====</p>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+				<p>=====</p>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+				<p>loc_1</p>
+				<p>loc_2</p>
+				<p>loc_3</p>
+				<p>loc_4</p>
+				<p>loc_5</p>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default SideNav;
