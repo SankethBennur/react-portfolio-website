15f84fe14960e1ab1ce26da69321a3f9077dfd25
SankethBennur <snkthbnnr3@gmail.com>
2023-01-30 00:13:35 +0530

    Added short summary in greetings section

--- src/components/sections/Greetings.jsx
+++ src/components/sections/Greetings.jsx
@@ -4,10 +4,38 @@ import "./Greetings.css";
 
 function Greetings() {
 	return (
-		<Grid id="greetings">
-			<h1 style={{ color: "lightGreen" }}>Greetings!</h1>
-			<p>I am Sanketh Bennur</p>
-			<br />
+		<Grid container id="greetings">
+			<Grid
+				item
+				xs={5}
+				sx={{
+					px: 5,
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "flex-end",
+					justifyContent: "auto",
+					// marginTop: "auto",
+				}}
+			>
+				<h1 style={{ color: "lightGreen" }}>Greetings!</h1>
+				<p>I am Sanketh Bennur</p>
+			</Grid>
+			<Grid
+				item
+				xs={7}
+				sx={{
+					borderLeft: 2,
+					pl: 3,
+					display: "flex",
+					flexDirection: "column",
+					// alignItems: "center",
+					margin: "auto",
+				}}
+			>
+				<div>Technology enthusiast</div>
+				<div>Enjoy great stories and Art</div>
+				<div>Love Adventures and Outdoors</div>
+			</Grid>
 		</Grid>
 	);
 }
