d46e03b785ea0d087f3d5eff3fdf27e773418023
SankethBennur <snkthbnnr3@gmail.com>
2023-01-30 14:44:46 +0530

    Responsive UI for mobile devices

---
+++ src/components/NavSections/ContactFooter.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+function ContactFooter() {
+	return (
+		<Grid sx={{ lineHeight: "15px", mb: 5 }}>
+			{/* Hardcoded: */}
+			<h3>Connect with me</h3>
+			<p>Phone:&nbsp;+91 6362 010 530</p>
+			<p>Email:&nbsp;sankethbennur@gmail.com</p>
+			<p>
+				Github:&nbsp;
+				<a
+					href={"https://github.com/sankethbennur"}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					github.com/sankethbennur
+				</a>
+			</p>
+			<p>
+				Instagram:&nbsp;
+				<a
+					href={"https://www.instagram.com/_lateralus_/"}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					@_lateralus_
+				</a>
+			</p>
+			<p>
+				Linkedin:&nbsp;
+				<a
+					href={"https://www.linkedin.com/in/sbennur"}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					linkedin.com/in/sbennur
+				</a>
+			</p>
+		</Grid>
+	);
+}
+
+export default ContactFooter;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import SideNav from "./SideNav";
+import { Grid, Drawer } from "@mui/material";
+import SideNav from "./NavSections/SideNav";
 
 function Navigation() {
 	return (
-		<Grid item xs={4}>
-			<SideNav />
+		// <Grid item xs={4}>
+		<Grid item xs={12} sm={4} md={4}>
+			<Grid
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					overflowY: "auto",
+					maxHeight: "100vh",
+					color: "rgba(0, 0, 0, 1)",
+				}}
+			>
+				<Drawer
+					variant="permanent"
+					sx={{
+						display: { xs: "none", sm: "block", md: "block" },
+						"& .MuiDrawer-paper": {
+							boxSizing: "border-box",
+							width: "33%",
+							backgroundColor: "rgba(0,0,0, 0)",
+							color: "rgba(255,255,255,1)",
+							border: 0,
+							px: 5,
+						},
+					}}
+					open
+				>
+					<SideNav />
+				</Drawer>
+			</Grid>
 		</Grid>
 	);
 }
